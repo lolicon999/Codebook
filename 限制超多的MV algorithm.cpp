3b8be98c5eb6d51f5d62b80587311b9fffae048a
rareone <rareone0602@me.com>
2016-09-29 15:54:34 +0800

    Update 限制超多的MV algorithm.cpp

--- 限制超多的MV algorithm.cpp
+++ 限制超多的MV algorithm.cpp
@@ -2,122 +2,127 @@
 #include <vector>
 #include <math.h>
 #include "memory.h"
-#define prime 4999999
-#define MAXN 500
+#define prime 1069
+#define MAXN 1000
 
 using namespace std;
-typedef vector<int> vi;
 typedef long long int lli;
-/*int V,ans=0,match[MAXN],Flower[MAXN],S[MAXN],vis[MAXN];
- 
- //4,999,999 is the 348513rd prime number.*/
+
+lli inverse(lli base,lli power=prime-2){
+    if( power==0 ) return 1;
+    else{
+        lli ans=inverse(base, power/2);
+        ans=(ans*ans)%prime*(power%2?base:1);
+        return ans%prime;
+    }
+}
+
+struct ffli{//finite field long long int
+    lli a;
+    ffli(lli b=0):a(b%prime){}
+    inline ffli operator=(const ffli b){return a=b.a;}
+    inline ffli operator*(const ffli b){return ffli((a*b.a)%prime);}
+    inline ffli operator/(const ffli b){return ffli((a*inverse(b.a))%prime);}
+    inline ffli operator+(const ffli b){return ffli((a+b.a)%prime);}
+    inline ffli operator-(const ffli b){return ffli((prime+(a-b.a))%prime);}
+    inline ffli operator%(const ffli b){return ffli(a%b.a);}
+    inline ffli operator+=(const ffli b){return *this=*this+b;}
+    inline ffli operator-=(const ffli b){return *this=*this-b;}
+    inline ffli operator-(){return ffli(prime-a);}
+    inline operator lli()const {return a;}
+    inline ffli operator[](ffli);
+};
+
+//4,999,999 is the 348513rd prime number.*/
 
 int V,E;
 int match[MAXN];
-lli M[MAXN][MAXN],I[MAXN][MAXN];
+ffli M[MAXN][MAXN],I[MAXN][MAXN];
 
-void Input(){
+inline int Input(){
     scanf("%d%d",&V,&E);
     for(int i=0;i<E;++i){
         int u,v;
         scanf("%d%d",&u,&v);--u,--v;
-        M[u][v]=rand()%100000%prime;
-        M[v][u]=prime-M[u][v];
+        M[u][v]=rand()%prime;
+        M[v][u]=-M[u][v];
     }
     for(int i=0;i<V;++i){
         for(int j=0;j<V;++j){
             I[i][j]=i==j;
         }
     }
+    return V;
 }
 
-lli inverse(lli base,lli power=prime-2){
-    static lli dp[prime]={0};
-    if(dp[base]) return dp[base];
-    if( power==0 ) return 1;
-    else{
-        lli ans=inverse(base, power/2);
-        ans=(ans*ans)%prime*(power%2?base:1);
-        dp[base]=ans%prime;
-        return dp[base];
-    }
-}
 
-void printboard(){
+inline void printboard(){
     for(lli i=0;i<V;++i){
-        printf("\n\n\n");
         for(lli j=0;j<V;++j)
-            printf("%4lld ",M[i][j]);
+            printf("%*lld ",(int)log(prime)-1,M[i][j]);
         printf(" | ");
         for(lli j=0;j<V;++j)
-            printf("%4lld ",I[i][j]);
-        printf("\n\n\n");
+            printf("%*lld ",(int)log(prime)-1,I[i][j]);
+        printf("\n");
     }
+    printf("\n");
 }
 
 
-void GaussEllimination(){
-    lli stay[V][V];
-    for(int i=0;i<V;++i)for(int j=0;j<V;++j) stay[i][j]=M[i][j],I[i][j]=i==j;//duplication
+int GaussEllimination(){
+    ffli S[MAXN][MAXN];
+    int rank=V;
+    for(int i=0;i<V;++i)for(int j=0;j<V;++j) S[i][j]=M[i][j],I[i][j]=i==j;//duplication
     for(int k=0;k<V;++k){
+        rank-=1;
         for(int i=k;i<V;++i){//The for loop will only excute O(V)
-            if(stay[i][k]!=0){
-                for(int j=0;j<V;++j) {swap(stay[i][j], stay[k][j]),swap(I[i][j], I[k][j]);}//O(V)
+            if(S[i][k]!=0){
+                for(int j=0;j<V;++j) {swap(S[i][j], S[k][j]),swap(I[i][j], I[k][j]);}//O(V)
                 for(int j=0;j<V;++j){
                     if(j==k)continue;
-                    int mul=(prime-stay[j][k])*inverse(stay[k][k])%prime;
+                    ffli mul=-S[j][k]/S[k][k];
                     for(int h=0;h<V;++h){
-                        stay[j][h]+=(stay[k][h]*mul)%prime,
-                        stay[j][h]%=prime;
-                        I[j][h]+=(I[k][h]*mul)%prime,
-                        I[j][h]%=prime;
+                        S[j][h]+=S[k][h]*mul,
+                        I[j][h]+=I[k][h]*mul;
                     }
-                }break;
+                }
+                rank+=1;
+                break;
             }
         }
-        int mul=inverse(stay[k][k])%prime;
+        ffli mul=ffli(1)/S[k][k];
         for(int h=0;h<V;++h){
-            stay[k][h]=mul*stay[k][h]%prime,I[k][h]=mul*I[k][h]%prime;
+            S[k][h]=mul*S[k][h],I[k][h]=mul*I[k][h];
         }
+        
+        printboard();
     }
+    return rank;
 }
 
 int MV_matching_algorithm(int V){
     int mat=0;
     for(int i=0;i<V;++i){
         for(int j=0;j<V;++j){//Eli col j
-            if(M[i][j]!=0 && I[j][i]!=0){
+            if(M[i][j]!=0 && I[j][i]!=0 && match[i]==-1 && match[j]==-1){
                 match[i]=j,match[j]=i,++mat;
-                for(int a=0;a<V;++a){
-                    if(a==i)continue;
-                    int mul=(prime-M[a][j])*inverse(M[i][j])%prime;
-                    for(int h=0;h<V;++h){
-                        M[a][h]=(M[a][h]+M[i][h]*mul)%prime,I[h][i]=(prime+(I[h][i]-I[h][a]*mul)%prime)%prime;
+                for(int h=0;h<2;++h,swap(i, j)){
+                    for(int a=0;a<V;++a){
+                        if(a==i)continue;
+                        ffli mul=-M[a][j]/M[i][j];
+                        for(int h=0;h<V;++h){
+                            M[a][h]=M[a][h]+M[i][h]*mul,I[h][i]=I[h][i]-I[h][a]*mul;
+                        }
+                    }
+                    for(int a=0;a<V;++a){
+                        if(a==j)continue;
+                        ffli mul=-M[i][a]/M[i][j];
+                        for(int h=0;h<V;++h){
+                            M[h][a]=M[h][a]+M[h][j]*mul,I[j][h]=I[j][h]-I[a][h]*mul;
+                        }
                     }
                 }
-                for(int a=0;a<V;++a){
-                    if(a==j)continue;
-                    int mul=(prime-M[i][a])*inverse(M[i][j])%prime;
-                    for(int h=0;h<V;++h){
-                        M[h][a]=(M[h][a]+M[h][j]*mul)%prime,I[j][h]=(prime+(I[j][h]-I[a][h]*mul)%prime)%prime;
-                    }
-                }
-                swap(i,j);
-                for(int a=0;a<V;++a){
-                    if(a==i)continue;
-                    int mul=(prime-M[a][j])*inverse(M[i][j])%prime;
-                    for(int h=0;h<V;++h){
-                        M[a][h]=(M[a][h]+M[i][h]*mul)%prime,I[h][i]=(prime+(I[h][i]-I[h][a]*mul)%prime)%prime;
-                    }
-                }
-                for(int a=0;a<V;++a){
-                    if(a==j)continue;
-                    int mul=(prime-M[i][a])*inverse(M[i][j])%prime;
-                    for(int h=0;h<V;++h){
-                        M[h][a]=(M[h][a]+M[h][j]*mul)%prime,I[j][h]=(prime+(I[j][h]-I[a][h]*mul)%prime)%prime;
-                    }
-                }
-                swap(i,j);
+                printboard();
                 break;
             }
         }
@@ -125,15 +130,25 @@ int MV_matching_algorithm(int V){
     return mat;
 }
 
+inline void join(int a){
+    for(int i=V;i<V+a;++i){
+        for(int j=0;j<V;++j){
+            M[i][j]=rand()%prime;
+            M[j][i]=-M[i][j];
+        }
+    }
+    V+=a;
+}
+
 int main(){
     memset(match,-1,sizeof(match));
-    Input();
-    GaussEllimination();
-    //printboard();
+    int vertice=Input();
+    int a=GaussEllimination();
+    printf("a = %d\n",a);
+    if(a!=V) {join(V-a);GaussEllimination();}
     int p=MV_matching_algorithm(V);
-    printf("%d\n",p%2==0?p/2:p/2+1);
-    for(int i=0;i<V;++i) printf("%d ",match[i]+1);
+    printf("p = %d\n",p);
+    printf("%d\n",a/2);
+    for(int i=0;i<vertice;++i) printf("%d ",(match[i]+1)>vertice?0:match[i]+1);
     cout<<endl;
-    
-    
 }
