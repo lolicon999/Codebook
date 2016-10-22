504214d1200542de767a1eee612acad99d28b874
rareone <rareone0602@me.com>
2016-10-22 17:20:22 +0800

    Update UF.cpp

--- UF.cpp
+++ UF.cpp
@@ -1,13 +1,11 @@
-#define MAXN 1000
-int P[MAXN],R[MAXN];
+#define MAXN 1000000
 int parent(int p){
     if(P[p]==p)return p;
     else return P[p]=parent(P[p]);
 }
 inline void UN(int a,int b){
-    if(R[parent(a)]<R[parent(b)])P[a]=P[b];
-    else{
-        R[P[b]]+=R[P[a]]==R[P[b]];
-        P[b]=P[a];
-    }
+    int A=parent(a),B=parent(b);
+    if(A==B) return;
+    if(R[A]<R[B]){P[A]=P[B];}
+    else R[A]+=R[A]==R[B],P[B]=P[A];
 }
