e6202f87a2db4fa80630a379e5271d4f12bea80c
rareone <rareone0602@me.com>
2016-10-22 13:31:35 +0800

    Update UF.cpp

--- UF.cpp
+++ UF.cpp
@@ -1,17 +1,13 @@
-#include <iostream>
-#include <vector>
-class UF{
-private:std::vector<int>  p,r;
-public:
-    int size;
-    UF(int i) {p.resize(size=i),r.assign(i,0); for(int j=0;j<i;++j)p[j]=j; }
-    int find(int i) {return p[i]==i?i:p[i]=find(p[i]);}
-    bool S(int i,int j){return find(i)==find(j);}
-    void US(int i,int j){
-        int x=find(i),y=find(j);
-        if(x!=y){--size;
-            if(r[x]>r[y])p[y]=x;
-            else{p[x]=y;r[y]+=(int)(r[x]==r[y]);}
-        }
+#define MAXN 1000
+int P[MAXN],R[MAXN];
+int parent(int p){
+    if(P[p]==p)return p;
+    else return P[p]=parent(P[p]);
+}
+inline void UN(int a,int b){
+    if(P[parent(a)]<R[parent(b)])P[a]=P[b];
+    else{
+        R[P[b]]+=R[P[a]]==R[P[b]];
+        P[b]=P[a];
     }
-};
+}
