f4e7d5bbeae6f7ebe35ab77bcdc4659bf84c7dd0
rareone <rareone0602@me.com>
2016-11-03 22:45:35 +0800

    Update and rename Classic LCA.cpp to Classic DP.cpp

--- Classic DP.cpp
+++ Classic DP.cpp
@@ -1,5 +1,6 @@
 #include <utility>
 #include <stack>
+#include <algorithm>
 using namespace std;
 typedef pair<int, int> ii;
 int DP[50][50]={0};
@@ -18,4 +19,22 @@ int LCS(int i,int j){
     }
         path[i][j]=ii(i-1,j);
         return DP[i][j]=LCS(i-1, j);
+}
+
+int s[100]={2,3,4,7,6};
+int pa[100]={0};
+int length[100]={1,0};
+int LMS(int n){
+    if(length[n]!=0)return length[n];
+    int max=1;
+    for(int i=0;i<n;++i){
+        if(s[i]<s[n])
+            if(LMS(i)+1>max)
+                max=LMS(i)+1,pa[n]=i;
+    }
+    return length[n]=max;
+}
+void back(int n){
+    if(pa[n]!=n)back(pa[n]);
+    printf("%d ",s[n]);
 }
